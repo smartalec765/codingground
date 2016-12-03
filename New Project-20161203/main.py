2dc9cc7d50f633ee67ee5c98bdcd02f875e2fa7f
smartalec765 <alecvgomez@gmail.com>
2016-12-02 23:17:49 -0800

    Update main.py

--- New Project-20161203/main.py
+++ New Project-20161203/main.py
@@ -1,3 +1,4 @@
+import webbrowser
 def inputExpenses(monthExpenses, months, categories):
     "This gets the expenses of the semenster"
     num = 0
@@ -8,34 +9,44 @@ def inputExpenses(monthExpenses, months, categories):
         num = 0
     "Prints out the total cost of the semester"
     for i in range(0, len(months)):
-        print "During the month of" ,months[i], "you spent" ,monthExpenses[i]
+        print "During the month of" , months[i] , "you spent", monthExpenses[i]
+        file.write("During the month of" 'months[i]'  "you spent" 'monthExpenses[i]')
     return
 
-def prediction(calculatedNum, studentIncome):
+def prediction(calculatedNum, studentIncome, file):
     if finalExpenses <= 99:
         adjustedIncome =(abs(calculatedNum) + abs(finalExpenses) + 300)
         print "In order to live comfortably next semester you will need " ,adjustedIncome, "."
+        file.write("In order to live comfortably next semester you will need " 'adjustedIncome' ".")
     elif finalExpenses >= 100:
         print "You are living comfortably at this rate! Keep up the great finance management"
+        file.write("You are living comfortably at this rate! Keep up the great finance management")
     return
 
-def outputExpenses(finalExpenses):
+def outputExpenses(finalExpenses, file):
     if finalExpenses > 0 and finalExpenses != 0:
         print "Your current fall semester balance is" ,finalExpenses
+        file.write("Your current fall semester balance is" 'finalExpenses')
     elif finalExpenses != 0 and finalExpenses < 0:
         print "You are in debt with" ,finalExpenses, "Dollars"
+        webbrowser.open("https://www.youtube.com/embed/mXYyEjRfO18")
         print "Watch this video for help on managing your finances: https://www.youtube.com/watch?v=mXYyEjRfO18"
+        file.write("You are in debt with" 'finalExpenses' "Dollars")
     elif finalExpenses == 0:
         print "You are broke with" ,finalExpenses, "Dollars."
         print "Watch this video for help on managing your finances: https://www.youtube.com/watch?v=mXYyEjRfO18"
+        webbrowser.open("https://www.youtube.com/embed/mXYyEjRfO18")
+        file.write("You are broke with" 'finalExpenses' "Dollars.")
     return
 
+file = open("Finances", 'w')
 monthExpenses = []
 global calculatedNum
 calculatedNum = 0
 months = ['August', 'September', 'October', 'November', 'December']
 categories = ['Food', 'Rent', 'Bills', 'Other']
 studentIncome = input("What is your income for the fall semester?")
+file.write('studenIncome')
 global finalExpenses
 finalExpenses = 0
 
@@ -44,5 +55,7 @@ for i in monthExpenses[:]:
     calculatedNum += int(i)
     finalExpenses = (studentIncome - calculatedNum)
 print "You spent $" , calculatedNum, "for this semester."
-outputExpenses(finalExpenses)
-prediction(calculatedNum, studentIncome)
+file.write("You spent $" 'calculatedNum' "for this semester.")
+outputExpenses(finalExpenses,file)
+prediction(calculatedNum, studentIncome, file)
+file.close()
